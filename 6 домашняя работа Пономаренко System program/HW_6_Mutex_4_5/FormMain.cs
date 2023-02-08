44ea359c58712fb9ed34d30b662e71c094bf5ea5
Max <91070715+MaxPanamax@users.noreply.github.com>
2023-02-08 15:37:48 +0200

    system_programming
    system_programming

---
+++ 6 домашняя работа Пономаренко System program/HW_6_Mutex_4_5/FormMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task4_5
+{
+    public partial class FormMain : Form
+    {
+        private string[] nameThread = { "Процесс заполнения файла числами", "Процесс перебора простых чисел", "Процесс перебора простых чисел по условию" };
+        public FormMain()
+        {
+            InitializeComponent();
+            buttonStart.Click += ButtonStart_Click;
+            buttonReportShow.Click += ButtonReportShow_Click;
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                buttonStart.Enabled = false;
+                Worker worker = new Worker();
+                worker.WorkBegin += Worker_WorkBegin;
+                worker.WorkEnd += Worker_WorkEnd;
+                Thread[] threads = new Thread[nameThread.Length];
+                for (int i = 0; i < threads.Length; i++)
+                {
+                    threads[i] = new Thread(worker.Start);
+                    threads[i].IsBackground = true;
+                    threads[i].Name = nameThread[i];
+                    threads[i].Start(i + 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Worker_WorkBegin(string info)
+        {
+            Action action = () => { listBoxLog.Items.Add(info); };
+            this.InvokeEx(action);
+        }
+        private void Worker_WorkEnd(string info)
+        {
+            Action action = () => { listBoxLog.Items.Add(info); };
+            this.InvokeEx(action);
+        }
+
+        private void ButtonReportShow_Click(object sender, EventArgs e)
+        {
+            dataGridViewReport.DataSource = null;
+            buttonStart.Enabled = true;
+            var sql = (from info in Worker.reports
+                       select new
+                       {
+                           Количество_чисел = info.countNumber,
+                           Размер_файла = info.sizeFile,
+                           Контент = info.fileСontent.Substring(0, 1000)
+                       }).ToList();
+            dataGridViewReport.DataSource = sql;
+        }
+    }
+}
---
+++ 6 домашняя работа Пономаренко System program/HW_6_Mutex_4_5/Worker.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task4_5
+{
+    public class Worker
+    {
+        public struct Report
+        {
+            public int countNumber;
+            public long sizeFile;
+            public string fileСontent;
+        }
+
+        private string[] fileNames = { "Thread1.txt", "Thread2.txt", "Thread3.txt" };
+        private Mutex mutex;
+        public static List<Report> reports { get; private set; }
+
+        public Worker()
+        {
+            mutex = new Mutex();
+        }
+
+        public void Start(object number)
+        {
+            mutex.WaitOne();
+            WorkBegin(Thread.CurrentThread.Name + " начал свою работу");
+            Thread.Sleep(3000);
+            switch ((int)number)
+            {
+                case 1: WriteNumberToFile();
+                        break;
+                case 2: ReadWritePrimeNumberToFile(fileNames[0], fileNames[1], false);
+                        break;
+                case 3: ReadWritePrimeNumberToFile(fileNames[1], fileNames[2], true);
+                        break;
+            }
+            WorkEnd(Thread.CurrentThread.Name + " завершил свою работу");
+            mutex.ReleaseMutex();
+            reports = ShowStatistic();
+        }
+
+        private List<Report> ShowStatistic()
+        {
+            reports = new List<Report>();
+            if (File.Exists(fileNames[0]) && File.Exists(fileNames[1]) && File.Exists(fileNames[2]))
+            {
+                for (int i = 0; i < fileNames.Length; i++)
+                {
+                    FileInfo fileInfo = new FileInfo(fileNames[i]);
+                    reports.Add(new Report
+                    {
+                        countNumber = File.ReadAllLines(fileNames[i]).Length,
+                        sizeFile = fileInfo.Length,
+                        fileСontent = File.ReadAllText(fileNames[i])
+                    });
+                }
+                return reports;
+            }
+            else
+                throw new FileNotFoundException("Нет необходимые файлов для генерации отчета");
+           
+        }
+
+        //выполнение первого потока
+        private void WriteNumberToFile()
+        {
+            Random random = new Random();
+            using (StreamWriter writer = new StreamWriter(fileNames[0], false))
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    int number = random.Next(1000);
+                    writer.WriteLine(number.ToString());
+                }
+            }
+        }
+        //Выполнение второго потока (additionalVerification==false) и третьего потока (additionalVerification==true)
+        private void ReadWritePrimeNumberToFile(string readerFile, string writeFile, bool additionalVerification)
+        {
+            using (StreamWriter writer = new StreamWriter(writeFile, false))
+            {
+                using (StreamReader reader = new StreamReader(readerFile))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (additionalVerification == false)
+                        {
+                            if (ulong.TryParse(line, out ulong number) & checkPrimeNumber(number))
+                            {
+                                writer.WriteLine(number.ToString());
+                            }
+                        }
+                        else
+                        {
+                            //Выполнение третьего потока с условием. 
+                            //Числа, наиденные во втором потоке уже по-любому простые.
+                            //Поэтому дополнительные проверок (ulong.TryParse(line, out ulong number) & checkPrimeNumber(number)) проводить нет смысла
+                            if (line[line.Length - 1] == '7')
+                            {
+                                writer.WriteLine(line);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //Функция проверки является ли число простым
+        private bool checkPrimeNumber(ulong number)
+        {
+            if (number < 2)
+                return false;
+            for (ulong i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public event Action<string> WorkBegin;
+        public event Action<string> WorkEnd;
+    }
+}
